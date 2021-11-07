3671e56ec81a745af22d3d8076d50fece85dc6de
Nick Rizzo <nickrizzosw@gmail.com>
2021-11-06 21:45:08 -0400

    [feat] python virtual environment & start script

---
+++ install/setup.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+# create virtual environment to install desired packages (i.e. flask & extensions)
+# Credit: Nick Rizzo's Personal Project: https://github.com/NRizzoInc/texting-and-emailing-agent/blob/develop/install/setup.sh
+[[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]] && isWindows=true || isWindows=false
+
+# if linux, need to check if using correct permissions
+if [[ "${isWindows}" = false ]]; then
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root ('sudo')"
+        exit
+    fi
+fi
+
+# CLI Flags
+print_flags () {
+    echo "========================================================================================================================="
+    echo "Usage: setup.sh"
+    echo "========================================================================================================================="
+    echo "Helper utility to setup everything to use this repo"
+    echo "========================================================================================================================="
+    echo "How to use:" 
+    echo "  To Start: ./setup.sh [flags]"
+    echo "========================================================================================================================="
+    echo "Available Flags (mutually exclusive):"
+    echo "    -a | --install-all (default): If used, install everything (recommended for fresh installs)"
+    echo "    -p | --python-packages: Update virtual environment with current python packages (this should be done on pulls)"
+    echo "    -h | --help: This message"
+    echo "========================================================================================================================="
+}
+
+# parse command line args
+upgradePkgs=true
+noneSet=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -a | --install-all )
+            upgradePkgs=true
+            noneSet=false
+            break
+            ;;
+        -p | --python-packages )
+            upgradePkgs=true
+            noneSet=false
+            break
+            ;;
+        -h | --help )
+            print_flags
+            exit 0
+            ;;
+        * )
+            echo "... Unrecognized Command: $1"
+            print_flags
+            exit 1
+    esac
+    shift
+done
+
+if [[ ${noneSet} == true ]]; then
+    print_flags
+    echo "Please use one of the flags (just use -a if you are unsure)"
+    exit
+fi
+
+
+THIS_FILE_DIR="$(readlink -fm $0/..)"
+virtualEnvironName="cs3200-venv"
+rootDir="$(readlink -fm ${THIS_FILE_DIR}/..)"
+srcDir="$(readlink -fm ${rootDir}/..)"
+backendDir=${srcDir}/backend
+installDir=${rootDir}/install
+virtualEnvironDir=${rootDir}/${virtualEnvironName}
+pythonVersion=3.9
+pipLocation="" # make global
+pythonLocation="" # global (changed based on OS)
+
+
+# check OS... (decide how to activate virtual environment)
+echo "#1 Setting up virtual environment"
+if [[ ${isWindows} = true ]]; then
+    # windows
+    echo "#1.1 Checking Python Version"
+    currVersionText=$(python3 --version)
+    currVersionMinor=$(echo "$currVersionText" | awk '{print $2}')
+    currVersion=$(echo "${currVersionMinor}" | sed -r 's/\.[0-9]$//') # strips away minor version (3.7.2 -> 3.7)
+
+    echo "Using python${currVersion} to build virtual environment"
+    if [[ ${pythonVersion} != ${currVersion} ]]; then
+        echo "WARNING: Wrong version of python being used. Expects python${pythonVersion}. This might affect results"
+    fi
+
+    echo "#1.2 Creating Virtual Environment"
+    py -m venv $virtualEnvironDir # actually create the virtual environment
+    $virtualEnvironDir/Scripts/activate
+    pipLocation=$virtualEnvironDir/Scripts/pip3.exe
+
+    echo "#1.3 Getting Path to Virtual Environment's Python"
+    pythonLocation=${virtualEnvironDir}/Scripts/python.exe
+    echo "-- pythonLocation: ${pythonLocation}"
+
+else
+    # linux
+    # needed to get specific versions of python
+    pythonName=python${pythonVersion}
+    echo "#1.1 Adding python ppa"
+    add-apt-repository -y ppa:deadsnakes/ppa
+
+    echo "#1.2 Updating..."
+    apt update -y
+
+    echo "#1.3 Upgrading..."
+    apt upgrade -y
+    apt install -y \
+        ${pythonName} \
+        ${pythonName}-venv
+
+    echo "#1.4 Creating Virtual Environment"
+    ${pythonName} -m venv ${virtualEnvironDir} # actually create the virtual environment
+    source ${virtualEnvironDir}/bin/activate
+    pipLocation=${virtualEnvironDir}/bin/pip${pythonVersion}
+
+    echo "#1.4.1 Getting Path to Virtual Environment's Python"
+    pythonLocation=${virtualEnvironDir}/bin/python # NOTE: don't use ".exe"
+    echo "-- pythonLocation: ${pythonLocation}"
+fi
+
+if [[ ${upgradePkgs} == true ]]; then
+    # update pip to latest
+    echo "#2 Upgrading pip to latest"
+    "${pythonLocation}" -m pip install --upgrade pip
+
+    # now pip necessary packages
+    echo "#3 Installing all packages"
+    "${pipLocation}" install -r "${installDir}"/requirements.txt
+fi
---
+++ start.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# this script enables user to run the code in this repo using the virtual environment
+# CREDIT: Nick Rizzo's Personal Project: https://github.com/NRizzoInc/texting-and-emailing-agent/blob/develop/start.sh
+
+# Get paths to everything
+virtualEnvironName="cs3200-venv"
+rootDir="$(readlink -fm $0/..)"
+virtualEnvironDir="${rootDir}/${virtualEnvironName}"
+srcPath="${rootDir}/src"
+backendPath="${srcPath}/backend"
+executePath="${backendPath}/main.py"
+setupScript="${rootDir}/install/setup.sh"
+
+# check OS... (decide how to call python)
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    # windows
+    venvPath=${virtualEnvironDir}/Scripts/python.exe
+else
+    # linux
+    venvPath=${virtualEnvironDir}/bin/python # NOTE: don't use ".exe"
+fi
+
+# CLI Flags
+print_flags () {
+    echo "=========================================================================================================="
+    echo "Usage: start.sh"
+    echo "=========================================================================================================="
+    echo "Helper utility to start up the Library Database Web Application"
+    echo "Starts up the virtual environment via bash and runs without user having to worry about it."
+    echo "=========================================================================================================="
+    echo "How to use:" 
+    echo "  To Start: ./start.sh"
+    echo "  To Stop: control+c"
+    echo "=========================================================================================================="
+    echo "Available Flags:"
+    echo "  -h,--help : Prints the command line help message of the native mode that is run"
+    echo "              (or this message if no mode specified)"
+    echo "=========================================================================================================="
+}
+
+# parse command line args
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        # none of these are reachable if mode is provided due to the break within the "mode" flag
+        -h | --help )
+            print_flags
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+# check if venv is setup, if not inform user to run install
+if [[ -f ${venvPath} ]]; then
+    echo "Using virtual environment ${venvPath}"
+else
+    echo "Virtual environment not setup. Run ${setupScript}"
+    exit
+fi
+
+
+# use "$@" to pass on all parameter the same way to python script
+echo "Starting Program ${executePath}"
+"${venvPath}" "${executePath}" "$@"
