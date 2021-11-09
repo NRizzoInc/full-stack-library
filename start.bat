73243428e0d68f175fb12515ce4a1ecf4bcb0ebb
Matt Rizzo <mattrizzo21@gmail.com>
2021-11-08 19:01:38 -0500

    added script to start from windows

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+::NOTE - This script will only work once the installation of the venv is completed using install.sh
+
+:: Get paths to everything
+Set virtualEnvironName=cs3200-venv
+Set root_dir=%~dp0
+
+Set executePath=%root_dir%src\backend\main.py
+Set virtualEnvironDir=%root_dir%%virtualEnvironName%
+Set venvPath=%virtualEnvironDir%\Scripts\python.exe
+
+
+echo Starting Program %executePath%
+%venvPath% %executePath% %*
+
