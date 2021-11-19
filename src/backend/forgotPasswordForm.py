65fd2092f72183acd98afde0f4388e30fbcdbed7
Nick Rizzo <nickrizzosw@gmail.com>
2021-11-18 23:28:07 -0500

    [feat] updated forgot password and added to links

--- src/backend/forgotPasswordForm.py
+++ src/backend/forgotPasswordForm.py
@@ -12,17 +12,17 @@ class ForgotPwdForm(FlaskForm, UserManager):
     """Generates a forgot password form to authenticate and change password"""
     #-----------------------------------Form Fields-----------------------------------#
     username = StringField('Username', validators=[DataRequired()])
-    card_num = IntegerField('Library Card#', validators=[DataRequired()])
+    card_num = IntegerField('Library Card #', validators=[DataRequired()])
     new_password = PasswordField('New Password',  validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Reset Password')
 
     def __init__(self, flaskApp: Flask, user_manager: UserManager, *args, **kwargs):
         FlaskForm.__init__(self, *args, **kwargs)
         self.user_manager = user_manager
         cls = self.__class__ # get reference to cls
         cls.username = StringField('Username', validators=[DataRequired()])
-        cls.card_num = StringField('Library Card#', validators=[DataRequired(), self.checkLibCard])
-        cls.new_password = PasswordField('New Password',  validators=[DataRequired()])
+        cls.card_num = StringField('Library Card #', validators=[DataRequired(), self.checkLibCard])
+        cls.new_password = PasswordField('Reset Password',  validators=[DataRequired()])
 
     def checkLibCard(self, form, field) -> bool:
         """Checks if username and library card match and are valid for a given user"""
