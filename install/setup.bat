51473335c94121173828db1d1f6881c9cecd4372
Matt Rizzo <mattrizzo21@gmail.com>
2021-12-01 17:25:37 -0500

    [fix] handle space pathing

--- install/setup.bat
+++ install/setup.bat
@@ -7,28 +7,28 @@ set installDir=%~dp0
 
 :: Need to do this syntax to get to parent (P) directory
 FOR %%P IN ("%installDir%.") DO SET rootDir=%%~dpP
-set virtualEnvironDir=%rootDir%%virtualEnvironName%
+set virtualEnvironDir="%rootDir%%virtualEnvironName%
 set pythonVersion=3.9
 
 :: Install the venv
 
 @echo Please use python3.9 when running this script...
-timeout 5 > NUL
+::timeout 5 > NUL
 
 @echo #1.1 Removing any existing environments
-RMDIR /S /Q "%virtualEnvironDir%"
+RMDIR /S /Q %virtualEnvironDir%"
 
 @echo on
 
 @echo #1.2: Creating Virtual Environment
 :: This will automatically activate the venv
 :: Specify the version (that should've been downloaded based on README instructions)
-py -3.9 -m venv "%virtualEnvironDir%"
+py -3.9 -m venv %virtualEnvironDir%"
 
 @echo #1.3 Getting Path to Virtual Environment's Python
-set pythonLocation="%virtualEnvironDir%\Scripts\python.exe"
-@echo -- pythonLocation: "%pythonLocation%"
-set pipLocation="%virtualEnvironDir%\Scripts\pip3.exe"
+set pythonLocation=%virtualEnvironDir%\Scripts\python.exe"
+@echo -- pythonLocation: %pythonLocation%
+set pipLocation=%virtualEnvironDir%\Scripts\pip3.exe"
 
 @echo #2 Upgrading pip to latest
 %pythonLocation% -m pip install --upgrade pip
@@ -37,7 +37,7 @@ set pipLocation="%virtualEnvironDir%\Scripts\pip3.exe"
 @echo #3 Installing all packages
 %pipLocation% install -r "%installDir%\requirements.txt"
 
-"%virtualEnvironDir%\Scripts\deactivate"
+%virtualEnvironDir%\Scripts\deactivate"
 
 @echo Install & Setup of the virtual environment for this project is done.
 @echo Please run the start.sh or start.bat files at the top level of the project.
